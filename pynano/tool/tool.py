edba78707e7a14e5f0377e5cd541b968ec5274d1
wnight <shaochuang_routine@outlook.com>
2019-02-06 09:44:19 +0800

    final update

--- pynano/tool/tool.py
+++ pynano/tool/tool.py
@@ -7,7 +7,7 @@
 from ui.tool_ui import *
 from PyQt5.QtWidgets import QMainWindow
 from tool.calPore import Cacl
-from tool.tdms import Tdms_read
+#from tool.tdms import Tdms_read
 
 
 class Analy_tool(QMainWindow, Ui_toolbox):
@@ -17,19 +17,19 @@ class Analy_tool(QMainWindow, Ui_toolbox):
         self.statusBar().showMessage("Ready")
 
         self.window1 = Cacl()
-        self.window2 = Tdms_read()
+        # self.window2 = Tdms_read()
         self.caclpore_window()
         self.poresize.clicked.connect(self.caclpore_window)
-        self.tdms.clicked.connect(self.tdms_window)
+        # self.tdms.clicked.connect(self.tdms_window)
 
     def caclpore_window(self):
-        self.window2.close()
+        # self.window2.close()
         self.verticalLayout_tool.addWidget(self.window1)
         self.window1.show()
         self.statusBar().showMessage("计算孔径")
 
-    def tdms_window(self):
-        self.window1.close()
-        self.verticalLayout_tool.addWidget(self.window2)
-        self.window2.show()
-        self.statusBar().showMessage("读取TDMS")
+    # def tdms_window(self):
+    #     self.window1.close()
+    #     self.verticalLayout_tool.addWidget(self.window2)
+    #     self.window2.show()
+    #     self.statusBar().showMessage("读取TDMS")
